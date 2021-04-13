3c849040f144feb7ff5d321c601305adc6b4dffd
Ankush Bose <ankushbose5@gmail.com>
2021-04-13 23:30:42 +0530

    fix: SaveReminderViewModelTest.kt added possibilities

--- starter/app/src/test/java/com/udacity/project4/viewmodel/SaveReminderViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/viewmodel/SaveReminderViewModelTest.kt
@@ -1,19 +1,23 @@
 package com.udacity.project4.viewmodel
 
+import android.content.Context
 import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
+import com.udacity.project4.R
 import com.udacity.project4.data.model.Reminder
 import com.udacity.project4.getOrAwaitValueTest
 import com.udacity.project4.view.locationreminders.data.FakeDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 import org.robolectric.annotation.Config
 
 /**
@@ -27,6 +31,8 @@ import org.robolectric.annotation.Config
 @Config(maxSdk = Build.VERSION_CODES.P, minSdk = Build.VERSION_CODES.P)
 class SaveReminderViewModelTest {
 
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
@@ -39,9 +45,13 @@ class SaveReminderViewModelTest {
             SaveReminderViewModel(ApplicationProvider.getApplicationContext(), FakeDataSource())
     }
 
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
 
     @Test
-    fun saveReminder_saveReminderListToLocal() = runBlockingTest {
+    fun saveReminder_saveReminderListToLocal_returnsTrue() = runBlockingTest {
 
         val reminder = Reminder(
             "Testing location",
@@ -56,7 +66,47 @@ class SaveReminderViewModelTest {
 
         val value = viewModel.isReminderSaved.getOrAwaitValueTest()
 
-        assertThat(value).isTrue()
+        val isLoading = viewModel.showLoading.getOrAwaitValueTest()
 
+        assertThat(isLoading).isFalse()
+        assertThat(value).isTrue()
+    }
+
+    @Test
+    fun saveReminder_saveReminderListToLocal_returnsEmptyTitle() = runBlockingTest {
+
+        val reminder = Reminder(
+            "",
+            "Sample descriptio",
+            "location name",
+            22.647596,
+            88.645856,
+            "123456"
+        )
+
+        viewModel.validateAndSaveReminder(reminder)
+
+        val value = viewModel.showMessage.getOrAwaitValueTest()
+
+        assertThat(value).contains(context.resources.getString(R.string.select_title))
+    }
+
+    @Test
+    fun saveReminder_saveReminderListToLocal_returnsEmptyLatOrLng() = runBlockingTest {
+
+        val reminder = Reminder(
+            "sample title",
+            "Sample descriptio",
+            "location name",
+            0.0,
+            88.645856,
+            "123456"
+        )
+
+        viewModel.validateAndSaveReminder(reminder)
+
+        val value = viewModel.showMessage.getOrAwaitValueTest()
+
+        assertThat(value).contains(context.resources.getString(R.string.select_location))
     }
 }
