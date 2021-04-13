58a4c911c7a93509e6ec080cf3201a4fcea0bafd
Ankush Bose <ankushbose5@gmail.com>
2021-04-14 04:56:31 +0530

    fix: Removed room db dependency from RemindersListViewModelTest.kt

--- starter/app/src/test/java/com/udacity/project4/viewmodel/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/viewmodel/RemindersListViewModelTest.kt
@@ -1,14 +1,9 @@
 package com.udacity.project4.viewmodel
 
-import android.content.Context
 import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
-import com.udacity.project4.data.local.RemindersDao
-import com.udacity.project4.data.local.RemindersDatabase
 import com.udacity.project4.data.model.Reminder
 import com.udacity.project4.view.locationreminders.data.FakeDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -33,20 +28,13 @@ class RemindersListViewModelTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private val context = ApplicationProvider.getApplicationContext<Context>()
-
-    lateinit var dao: RemindersDao
-
     lateinit var viewModel: RemindersListViewModel
+
+    private val repository = FakeDataSource()
 
     @Before
     fun setUp() {
-        val db = Room.inMemoryDatabaseBuilder(context, RemindersDatabase::class.java)
-            .allowMainThreadQueries().build()
-
-        dao = db.reminderDao()
-
-        viewModel = RemindersListViewModel(FakeDataSource())
+        viewModel = RemindersListViewModel(repository)
     }
 
 
@@ -62,7 +50,7 @@ class RemindersListViewModelTest {
             "123456"
         )
 
-        dao.saveReminder(reminder)
+        repository.saveReminder(reminder)
 
         val listOfReminders = viewModel.remindersList
 
